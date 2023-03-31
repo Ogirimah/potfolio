386181ba3d22f8932ec8c04b4b3cdc5e8131fee5
Ameer Ogirimah <ao22abk@herts.ac.uk>
2023-04-01 00:43:50 +0100

    Add fontawesome to the project

--- new/src/components/Main.js
+++ new/src/components/Main.js
@@ -1,10 +1,22 @@
 import React from "react";
 
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
 export default function Navbar() {
     return (
     <body>
     <section>
-        <div></div>
+        <div>
+          <ul>
+            <li><FontAwesomeIcon icon={("linkedin")} /></li>
+            <li><FontAwesomeIcon icon={("github")} /></li>
+            <li></li>
+            <li><FontAwesomeIcon icon={("codepen")} /></li>
+            <li><FontAwesomeIcon icon={("twitter")} /></li>
+            <li><FontAwesomeIcon icon={("facebook")} /></li>
+            <li><FontAwesomeIcon icon={("instagram")} /></li>
+          </ul>
+        </div>
         <div></div>
         <div></div>
     </section>
