66b0728f1e5bb788639fd4d26f909e77e6d9246a
Ameer Ogirimah <ao22abk@herts.ac.uk>
2023-04-06 04:24:07 +0100

    Scratch sheet for implementing dark mode

--- new/src/App.js
+++ new/src/App.js
@@ -1,7 +1,10 @@
 import './App.css';
+import './theme.css';
 import Navbar from './components/Navbar';
 import Main from './components/Main';
 import Footer from './components/Footer';
+import { createContext, useState } from 'react';
+
 
 // Font Awesome
 import { library } from '@fortawesome/fontawesome-svg-core';
@@ -10,14 +13,24 @@ import { fas } from '@fortawesome/free-solid-svg-icons';
 
 library.add(fab, fas);
 
+export const ThemeContext = createContext(null);
+
 
 function App() {
+  const [theme, setTheme] = useState('light');
+
+  const toggleTheme = () => {
+    setTheme(theme === 'dark' ? 'light' : 'dark');
+  };
+
   return (
-    <div className="App">
+    <ThemeContext.Provider value={{ theme, toggleTheme }}>
+    <div className="App" id='{theme}'>
       <Navbar />
       <Main />
       <Footer />
     </div>
+    </ThemeContext.Provider>
   );
 }
 
