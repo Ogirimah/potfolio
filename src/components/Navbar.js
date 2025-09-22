7552ad1742db8841a4538a374b10c35487610ae0
Ogirimah <ameerogirimah@outlook.com>
2025-09-22 14:12:10 +0100

    Revert "Revert "Revert "Fix mobile menu broken functionality, and update resume"""
    This reverts commit 190ba6b6205d7db3f4a3c8d437df57e7587e1794.

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,43 +1,29 @@
-// Navbar.js
-import { useContext, useState } from "react";
+import { useContext } from "react";
 import styles from "../styles/navbar.module.css";
 import { ThemeContext } from "../App.js"
 
 export default function Navbar() {
     const { theme } = useContext(ThemeContext);
-    const [isMenuOpen, setIsMenuOpen] = useState(false);
-
-    const toggleMenu = () => {
-        setIsMenuOpen(!isMenuOpen);
-    };
-
-    const closeMenu = () => {
-        setIsMenuOpen(false);
-    };
 
     return (
         <div className={styles.container}>
             <header className={styles.heading}>
                 <h1>
-                    <a href='https://ogirimah.com' onClick={closeMenu}>Ameer</a>
+                    <a href='https://ogirimah.com'>Ameer</a>
                 </h1>
 
-                <ul className={`${styles.list} ${isMenuOpen ? styles.active : ''}`}>
-                    <li><a href='#about' onClick={closeMenu}>About</a></li>
-                    <li><a href='#techStack' onClick={closeMenu}>Tech Stack</a></li>
-                    <li><a href='#projects' onClick={closeMenu}>Projects</a></li>
-                    <li><a href='#workExperience' onClick={closeMenu}>Experience</a></li>
-                    <li><a href='#contact' onClick={closeMenu}>Contact</a></li>
+                <ul className={styles.list}>
+                    <li><a href='#about'>About</a></li>
+                    <li><a href='#techStack'>Tech Stack</a></li>
+                    <li><a href='#projects'>Projects</a></li>
+                    <li><a href='#workExperience'>Experience</a></li>
+                    <li><a href='#contact'>Contact</a></li>
                 </ul>
 
-                <div
-                    className={styles.mobileMenu}
-                    onClick={toggleMenu}
-                    aria-label="Toggle menu"
-                >
-                    <span className={isMenuOpen ? styles.open : ''}></span>
-                    <span className={isMenuOpen ? styles.open : ''}></span>
-                    <span className={isMenuOpen ? styles.open : ''}></span>
+                <div className={styles.mobileMenu}>
+                    <span></span>
+                    <span></span>
+                    <span></span>
                 </div>
             </header>
         </div>
