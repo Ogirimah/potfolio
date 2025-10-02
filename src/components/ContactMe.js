215b5b36d81fb3cf6a79265721d3a3311f06ce13
Ogirimah <ameerogirimah@outlook.com>
2025-10-02 08:49:45 +0100

    Complete mobile menu functionality, Im prove footer responsiveness, fix contact me functionality, and improve social icons' responsiveness

--- src/components/ContactMe.js
+++ src/components/ContactMe.js
@@ -7,78 +7,78 @@ const ContactMe = () => {
     const [name, setName] = useState('');
     const [message, setMessage] = useState('');
 
-    const handleSubmit = async (event) => {
-        event.preventDefault();
+    // const handleSubmit = async (event) => {
+    //     event.preventDefault();
 
-        try {
-            const response = await fetch('"https://formbold.com/s/ozKj8"', {
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json',
-                },
-                body: JSON.stringify({
-                    "email": email,
-                    "name": name,
-                    "message": message
-                }),
-            });
+    //     try {
+    //         const response = await fetch('"https://formbold.com/s/9BqDQ"', {
+    //             method: 'POST',
+    //             headers: {
+    //                 'Content-Type': 'application/json',
+    //             },
+    //             body: JSON.stringify({
+    //                 "email": email,
+    //                 "name": name,
+    //                 "message": message
+    //             }),
+    //         });
 
-            if (!response.ok) {
-                throw new Error('Network response was not ok');
-            }
+    //         if (!response.ok) {
+    //             throw new Error('Network response was not ok');
+    //         }
 
-        } catch (error) {
-            alert(`Failed to send message: ${error}`);
-        }
-    };
+    //     } catch (error) {
+    //         alert(`Failed to send message: ${error}`);
+    //     }
+    // };
 
     return (
         <div>
-            <div className={styles.overlay}>
-                <div className={styles.modal}>
-                    <h2>Submit your query here</h2>
-                    <form onSubmit={handleSubmit} method='POST'>
-                        <label htmlFor="names" className={styles.label}>
-                            Name
-                            <input
-                                type="text"
-                                value={name}
-                                name='names'
-                                onChange={(e) => setName(e.target.value)}
-                                placeholder="Name"
-                                required
-                                className={styles.input}
-                            />
-                        </label>
-                        <label htmlFor="email" className={styles.label}>
-                            Email
-                            <input
-                                type="email"
-                                value={email}
-                                name='email'
-                                onChange={(e) => setEmail(e.target.value)}
-                                placeholder="Email"
-                                required
-                                className={styles.input}
-                            />
-                        </label>
-                        <label htmlFor='text' className={styles.label}>
-                            Message
-                            <textarea
-                                value={message}
-                                name='message'
-                                onChange={(e) => setMessage(e.target.value)}
-                                placeholder="Tyope your message here..."
-                                className={styles.input}
-                            ></textarea>
-                        </label>
-                        <span>
-                            <button type="submit" className={styles.submitButton}>
-                                Submit
-                            </button>
-                        </span>
-                    </form>
-                </div>
+            <div className={styles.modal}>
+                <h2>Submit your query here</h2>
+                {/* <form onSubmit={handleSubmit}> */}
+                <form action="https://formbold.com/s/9BqDQ" method='POST' encType="multipart/form-data">
+                    <label htmlFor="names" className={styles.label}>
+                        Name
+                        <input
+                            type="text"
+                            value={name}
+                            name='names'
+                            onChange={(e) => setName(e.target.value)}
+                            placeholder="Name"
+                            required
+                            className={styles.input}
+                        />
+                    </label>
+                    <label htmlFor="email" className={styles.label}>
+                        Email
+                        <input
+                            type="email"
+                            value={email}
+                            name='email'
+                            onChange={(e) => setEmail(e.target.value)}
+                            placeholder="Email"
+                            required
+                            className={styles.input}
+                        />
+                    </label>
+                    <label htmlFor='text' className={styles.label}>
+                        Message
+                        <textarea
+                            type="textarea"
+                            value={message}
+                            name='message'
+                            onChange={(e) => setMessage(e.target.value)}
+                            placeholder="Type your message here..."
+                            className={styles.input}
+                        ></textarea>
+                    </label>
+                    <span>
+                        <button type="submit" className={styles.submitButton}>
+                            Submit
+                        </button>
+                    </span>
+                </form>
             </div>
         </div>
     );
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,9 +1,15 @@
 import { useContext } from "react";
 import styles from "../styles/navbar.module.css";
 import { ThemeContext } from "../App.js"
+import { useState } from "react";
 
 export default function Navbar() {
     const { theme } = useContext(ThemeContext);
+
+    const [isMenuOpen, setIsMenuOpen] = useState(false);
+    const toggleMenu = () => {
+        setIsMenuOpen(!isMenuOpen);
+    }
 
     return (
         <div className={styles.container}>
@@ -20,12 +26,21 @@ export default function Navbar() {
                     <li><a href='#contact'>Contact</a></li>
                 </ul>
 
-                <div className={styles.mobileMenu}>
+                <div onClick={toggleMenu} className={styles.mobileMenu}>
                     <span></span>
                     <span></span>
                     <span></span>
                 </div>
+
+
             </header>
+            <ul className={styles.menuList + (isMenuOpen ? ` ${styles.active}` : '')}>
+                <li><a href='#about'>About</a></li>
+                <li><a href='#techStack'>Tech Stack</a></li>
+                <li><a href='#projects'>Projects</a></li>
+                <li><a href='#workExperience'>Experience</a></li>
+                <li><a href='#contact'>Contact</a></li>
+            </ul>
         </div>
     );
 }
